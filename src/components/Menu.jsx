ba10993ad49ae8ae45e315bc81d86c58bbaa3820
Bolaji Dawodu <dawodubolaji189@gmail.com>
2025-08-06 13:25:13 +0100

    Implement the Art Section

---
+++ src/components/Menu.jsx
@@ -0,0 +1,12 @@
+
+
+const Menu = () => {
+  return (
+    <section id="menu" aria-labelledby="menu-heading">
+        <img src="/images/slider-right-leaf.png" alt="left-leaf" id="m-left-leaf" />
+        <img src="/images/slider-left-leaf.png" alt="left-leaf" id="m-right-leaf" />
+    </section>
+  )
+}
+
+export default Menu
